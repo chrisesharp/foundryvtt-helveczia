6a47d505571e95394630e9909852f78db12c0349
Chris Sharp <chrisesharp@gmail.com>
2024-01-23 16:36:09 +0000

    fix names test

--- src/tests/names.test.ts
+++ src/tests/names.test.ts
@@ -18,40 +18,42 @@ export function nameTests(context) {
       { sex: 'female', people: 'italian', needle: '<h1 class="generated-name">' },
       { sex: 'male', people: 'german', needle: '<h1 class="generated-name">' },
     ].forEach(({ sex, people, needle }) => {
-      it(`render ${people} ${sex} name to Chat`, async () => {
-        // Hook to capture when our dialog has actually rendered
-        const rendered = $.Deferred();
-        Hooks.once('renderApplication', (...args) => rendered.resolve(args));
+      describe(`Render ${sex} ${people} name`, async () => {
+        it(`render ${people} ${sex} name to Chat`, async () => {
+          // Hook to capture when our dialog has actually rendered
+          const rendered = $.Deferred();
+          Hooks.once('renderApplication', (...args) => rendered.resolve(args));
 
-        const dialog = await HVNameGenerator.showDialog();
+          const dialog = await HVNameGenerator.showDialog();
 
-        const [application, $firstHtml] = await rendered.promise();
+          const [application, $firstHtml] = await rendered.promise();
 
-        // Sanity check the renderApplication hook returned our dialog
-        expect(dialog).to.equal(application);
+          // Sanity check the renderApplication hook returned our dialog
+          expect(dialog).to.equal(application);
 
-        // Hook to capture when new name generator dialog has been rendered
-        const created = $.Deferred();
-        Hooks.once('renderApplication', (...args) => created.resolve(args));
+          // Hook to capture when new name generator dialog has been rendered
+          const created = $.Deferred();
+          Hooks.once('renderApplication', (...args) => created.resolve(args));
 
-        // Dialog API does not expose methods for selection, using dom instead
-        $firstHtml.find('select#sex').val(sex);
-        $firstHtml.find('select#people').val(people);
-        $firstHtml.find('button.dialog-button.ok').click();
+          // Dialog API does not expose methods for selection, using dom instead
+          $firstHtml.find('select#sex').val(sex);
+          $firstHtml.find('select#people').val(people);
+          $firstHtml.find('button.dialog-button.ok').click();
 
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const [_, $secondHtml] = await created.promise();
-        const generatedName = $secondHtml.find('h1.generated-name').text();
-        await $secondHtml.find('button.dialog-button.ok').click();
-        expect(generatedName.length).to.be.above(0);
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const [_, $secondHtml] = await created.promise();
+          const generatedName = $secondHtml.find('h1.generated-name').text();
+          await $secondHtml.find('button.dialog-button.ok').click();
+          expect(generatedName.length).to.be.above(0);
 
-        // Hook to capture when new Chat Message has been rendered
-        const messageCreated = $.Deferred();
-        Hooks.once('renderChatMessage', (...args) => messageCreated.resolve(args));
-        const [message] = await messageCreated.promise();
-        expect(message.content).to.have.string(needle);
-        expect(message.content).to.have.string(generatedName);
-        message.delete();
+          // Hook to capture when new Chat Message has been rendered
+          const messageCreated = $.Deferred();
+          Hooks.once('renderChatMessage', (...args) => messageCreated.resolve(args));
+          const [message] = await messageCreated.promise();
+          expect(message.content).to.have.string(needle);
+          expect(message.content).to.have.string(generatedName);
+          message.delete();
+        });
       });
     });
   });
