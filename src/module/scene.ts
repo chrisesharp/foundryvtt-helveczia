cdb972a9ddf11485ca369eafc020f5afe66a0410
Chris Sharp <chrisesharp@gmail.com>
2024-09-22 15:54:38 +0100

    Fix scaling settings

--- src/module/scene.ts
+++ src/module/scene.ts
@@ -3,19 +3,20 @@ export class HVSceneConfig extends SceneConfig {
     const $html = await super._renderInner(data);
     if (game.settings.get('helveczia', 'token-depth')) {
       // add scene config for scale factor
-      const scale = (this.object as Scene)?.flags?.helveczia?.scale ?? CONFIG.HV.DEFAULT_SCENE_SCALE;
-      const elevation = (this.object as Scene)?.flags?.helveczia?.elevation ?? CONFIG.HV.DEFAULT_ELEVATION;
+      const scene = data.document as Scene;
+      const scale = parseFloat(scene.flags?.helveczia?.scale ?? CONFIG.HV.DEFAULT_SCENE_SCALE);
+      const elevation = parseInt(scene.flags?.helveczia?.elevation ?? CONFIG.HV.DEFAULT_ELEVATION);
       const scaling = $(`<hr><div class="form-group">
           <label>${game.i18n.localize('HV.Setting.token.Scale')}</label>
           <div class="form-fields">
-            <input type="text" name="flags.helveczia.scale" value="${scale}"/>
+            <input type="number" name="flags.helveczia.scale" value="${scale}"/>
           </div>
           <p class="notes">
           ${game.i18n.localize('HV.Setting.token.ScaleHint')}
           </p>
           <label>${game.i18n.localize('HV.Setting.token.DepthDefault')}</label>
           <div class="form-fields">
-            <input type="text" name="flags.helveczia.elevation" value="${elevation}"/>
+            <input type="number" name="flags.helveczia.elevation" value="${elevation}"/>
           </div>
           <p class="notes">
           ${game.i18n.localize('HV.Setting.token.DepthDefaultHint')}
